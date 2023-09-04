b94154cab29679272c7a1e8a3f74b7f8c7ef9860
jacopaghera <123456789+jacopaghera@gmail.com>
2023-09-04 17:45:43 +0200

    modified the returns of the roundGame function

--- script.js
+++ script.js
@@ -4,47 +4,44 @@ function getComputerChoice() {
     return randomChoice;
 }
 
-console.log(getComputerChoice())
-
-
 function roundGame(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === "rock") {
         if (computerSelection === "Scissors") {
             playerScore++;
-            return "You win the round!";
+            return "You win the round! Rock beats scissors";
         }
         else if (computerSelection === "Rock") {
-            computerScore++
+            computerScore++;
             return "Retry!";
         }
         else {
-            return "You lose!";
+            return "You lose! Paper beats rock";
         }
     }
     else if (playerSelection.toLowerCase() === "paper") {
         if (computerSelection === "Rock") {
             playerScore++;
-            return "You win the round!";
+            return "You win the round! Paper beats rock";
         }
         else if (computerSelection === "Paper") {
             computerScore++;
             return "Retry!";
         }
         else {
-            return "You lose!";
+            return "You lose! Scissors beat paper";
         }
     }
     else if (playerSelection.toLowerCase() === "scissors") {
         if (computerSelection === "Paper") {
             playerScore++;
-            return "You win the round!";
+            return "You win the round! Scissors beat paper";
         }
         else if (computerSelection === "Scissors") {
             computerScore++;
             return "Retry!";
         }
         else {
-            return "You lose!";
+            return "You lose! Rock beats scissors";
         }
     }
     else {
@@ -52,24 +49,23 @@ function roundGame(playerSelection, computerSelection) {
     }
 }
 
-let playerScore = 0;
-let computerScore = 0;
-const playerSelection = prompt("Insert input here");
-const computerSelection = getComputerChoice();
-
 function game() {
-    roundGame.call(playerSelection, computerSelection);
-    roundGame.call(playerSelection, computerSelection);
-    roundGame.call(playerSelection, computerSelection);
-    roundGame.call(playerSelection, computerSelection);
-    roundGame.call(playerSelection, computerSelection);
+    getComputerChoice();
+    roundGame(playerSelection, computerSelection);
+    getComputerChoice();
+    roundGame(playerSelection, computerSelection);
+    getComputerChoice();
+    roundGame(playerSelection, computerSelection);
+    getComputerChoice();
+    roundGame(playerSelection, computerSelection);
+    getComputerChoice();
+    roundGame(playerSelection, computerSelection);
 
-    if (playerScore > computerScore) {
-        return ("You win!");
-    }
-    else {
-        return ("You lose!");
-    }
 }
 
-console.log(game());
+let playerScore = 0;
+let computerScore = 0;
+const playerSelection = prompt("Insert your answer");
+const computerSelection = getComputerChoice();
+
+console.log(roundGame(playerSelection, computerSelection))
